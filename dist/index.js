f1852d6774b2702e392d0c690627243d3fd529f4
Kamil Mrzygłód <kamil@thecloudtheory.com>
2024-01-04 20:34:49 +0100

    New version

--- dist/index.js
+++ dist/index.js
@@ -30836,6 +30836,86 @@ try {
         throw new Error('Please provide a valid input.');
     }
 
+    if(core.getInput('deployment-mode')) {
+        command += ' --mode ' + core.getInput('mode');
+    }
+
+    if(core.getInput('threshold')) {
+        command += ' --threshold ' + core.getInput('threshold');
+    }
+
+    if(core.getInput('parameters-file')) {
+        command += ' --parameters ' + core.getInput('parameters-file');
+    }
+
+    if(core.getInput('currency')) {
+        command += ' --currency ' + core.getInput('currency');
+    }
+
+    if(core.getInput('generate-json-output')) {
+        command += ' --generateJsonOutput ' + core.getInput('generate-json-output');
+    }
+
+    if(core.getInput('silent')) {
+        command += ' --silent ' + core.getInput('silent');
+    }
+
+    if(core.getInput('stdout')) {
+        command += ' --stdout ' + core.getInput('stdout');
+    }
+
+    if(core.getInput('disable-detailed-metrics')) {
+        command += ' --disableDetailedMetrics ' + core.getInput('disable-detailed-metrics');
+    }
+
+    if(core.getInput('json-output-filename')) {
+        command += ' --jsonOutputFilename ' + core.getInput('json-output-filename');
+    }
+
+    if(core.getInput('generate-html-output')) {
+        command += ' --generateHtmlOutput ' + core.getInput('generate-html-output');
+    }
+
+    if(core.getInput('output-format')) {
+        command += ' --outputFormat ' + core.getInput('output-format');
+    }
+
+    if(core.getInput('disable-cache')) {
+        command += ' --disable-cache ' + core.getInput('disable-cache');
+    }
+
+    if(core.getInput('terraform-executable-path')) {
+        command += ' --tf-executable ' + core.getInput('terraform-executable-path');
+    }
+
+    if(core.getInput('conversion-rate')) {
+        command += ' --conversion-rate ' + core.getInput('conversion-rate');
+    }
+
+    if(core.getInput('cache-handler')) {
+        command += ' --cache-handler ' + core.getInput('cache-handler');
+    }
+
+    if(core.getInput('cache-storage-account-name')) {
+        command += ' --cache-storage-account-name ' + core.getInput('cache-storage-account-name');
+    }
+
+    if(core.getInput('webhook-url')) {
+        command += ' --webhook-url ' + core.getInput('webhook-url');
+    }
+
+    if(core.getInput('webhook-authorization')) {
+        command += ' --webhook-authorization ' + core.getInput('webhook-authorization');
+    }
+
+    if(core.getInput('inline')) {
+        const parameters = core.getInput('inline');
+        for (const [key, value] of Object.entries(parameters)) {
+            command += ' --inline ' + key + '=' + value;
+        }
+    }
+
+
     const result = execSync(command).toString();
     console.log(result);
 } catch (error) {
